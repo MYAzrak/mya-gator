7b5dc1fd2b079facb3e76aafdf7a52916a4ef80e
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2026-08-23 21:09:41 +0400

    Add handler for unfollowing feeds

--- handler_feed_follow.go
+++ handler_feed_follow.go
@@ -59,3 +59,27 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 
 	return nil
 }
+
+// handlerUnfollow allows the current user to unfollow a feed by its URL.
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <url>", cmd.Name)
+	}
+	url := cmd.Args[0]
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't delete feed follow: %w", err)
+	}
+
+	fmt.Println(user.Name, "is no longer following", feed.Name)
+	return nil
+}
